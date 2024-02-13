2d4e0375f3a2150d8d71108d83fefe2ab4fb4717
LuizVictorr <luizvictortmc@gmail.com>
2024-02-13 16:30:02 -0300

    inserindo a lógica multiplicativa das proporções relacionadas a quantidade de reações

--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,10 @@ const Volumetria = () => {
   const [mols, setMols] = useState('')
   const [volumeGasto, setVolumeGasto] =  useState('');
   const [volumeSolução, setVolumeSolução] = useState('');
-  const [resultado, setResultado] = useState('');
   const [numInputs, setNumInputs] = useState('');
-  const [inputsValues, setInputsValues] = useState([]);
-
-  const handleChangeNumInputs = (e) => {
-    setNumInputs(parseInt(e.target.value));
-  };
-
-  const handleGenerateInputs = () => {
-    // Verifica se o número é menor ou igual a zero ou se ele não é inteiro, caso seja true retorna zero
-    if (numInputs <= 0 || !Number.isInteger(numInputs)) {
-      return null;
-    }
-  
-    // Gera um array com a quantidade de numImpunts, onde cada index representa 1 input criado pela tag html
-    const newInputs = Array.from({ length: numInputs }, (_, index) => (
-      <div>
-        <label>Proporção {index + 1}</label>
-        <input key={index} type="text" />
-        :
-        <input key={index} type="text" />
-      </div>
-    ));
-    return newInputs;
-  };
+  const [InputValuesLeft, setInputValuesLeft] = useState([]);
+  const [InputValuesRight, setInputValuesRight] = useState([]);
+  const [volumetriaResultado, setVolumetriaResultado] = useState('');
 
   const handleChangeMols = (e) => {
     setMols(e.target.value);
@@ -41,12 +20,66 @@ const Volumetria = () => {
 
   const handleChangeVolumeSolução = (e) => {
     setVolumeSolução(e.target.value);
-  }
+  };
 
-  const volumetriaNeutralização = () => {
-    const vn = (mols * volumeGasto) / volumeSolução
-    setResultado(vn)
-  }
+  // Coleta a quantidade de inputs que vão ser inseridos posteriormente
+  const handleChangeNumInputs = (e) => {
+    setNumInputs(parseInt(e.target.value));
+  };
+
+  // Coleta as informações do array de input da esquerda
+  const handleInputChangeLeft = (index, e) => {
+      const newInputValuesLeft = [...InputValuesLeft];
+      newInputValuesLeft[index] = e.target.value;
+      setInputValuesLeft(newInputValuesLeft);
+  };
+
+  // Coleta as informações do array de input da direita
+  const handleInputChangeRight = (index, e) => {
+      const newInputValuesRight = [...InputValuesRight];
+      newInputValuesRight[index] = e.target.value;
+      setInputValuesRight(newInputValuesRight);
+  };
+
+  //Gera os inputs no formato []:[]
+  const handleGenerateInputs = () => {
+      if (numInputs <= 0 || !Number.isInteger(numInputs)) {
+          return null;
+      }
+
+      const newInputs = Array.from({ length: numInputs }, (_, index) => (
+          <div key={index}>
+              <label>Proporção {index + 1}</label>
+              <input type="text" onChange={(e) => handleInputChangeLeft(index, e)} />
+              :
+              <input type="text" onChange={(e) => handleInputChangeRight(index, e)} />
+          </div>
+      ));
+      return newInputs;
+  };
+
+  // Multiplica os valores do array de input da esquerda
+  const multiplyValuesLeft = () => {
+      const valuesLeft = InputValuesLeft.map(value1 => parseFloat(value1));
+      const resultLeft = valuesLeft.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+      return resultLeft;
+  };
+
+  // Multiplica os valores do array de input da direita
+  const multiplyValuesRight = () => {
+      const valuesRight = InputValuesRight.map(value2 => parseFloat(value2));
+      const resultRight = valuesRight.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+      return resultRight;
+  };
+
+  // Faz o cálculo da volumetria
+  const volumetriaNeutralizacao = () => {
+    const resultLeft = multiplyValuesLeft();
+    const resultRight = multiplyValuesRight();
+    const vn = (mols * volumeGasto * resultRight) / (volumeSolução * resultLeft);
+    setVolumetriaResultado(vn.toFixed(4));
+    return vn;
+  };
 
   return (
     <div>
@@ -62,21 +95,24 @@ const Volumetria = () => {
         <label>Volume da Solução</label>
       <input type="number" value={volumeSolução} onChange={handleChangeVolumeSolução}/>
       </div>
-      <button onClick={volumetriaNeutralização}>Calcular</button>
-      <p>{resultado} Mol/L</p>
-
-      
       <div>
-        <label>Número de Reações</label>
-        <input type="number" value={numInputs} onChange={handleChangeNumInputs}/>  
-      </div>
-      
-      {numInputs > 0 && (
-        <div>
-          {handleGenerateInputs()}
-        </div>
-      )}
+          <label>Número de Reações</label>
+            <input type="number" value={numInputs} onChange={handleChangeNumInputs} />
+          </div>
 
+          {numInputs > 0 && (
+            <div>
+              {handleGenerateInputs()}
+            </div>
+          )}
+
+          <button onClick={volumetriaNeutralizacao}>Calcular</button>
+
+          {volumetriaResultado !== null && (
+            <div>
+              Resultado final: {volumetriaResultado}
+            </div>
+          )}
     </div>
   );
 };
